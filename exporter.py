3f649ac8fa23f162e3807a314f0b8f09371f3347
EC2 Default User <ec2-user@ip-172-31-27-69.ap-northeast-1.compute.internal>
2024-01-30 10:41:33 +0000

    1

--- exporter.py
+++ exporter.py
@@ -1,9 +1,13 @@
+# Buffaloのルータの管理画面からパケット数のページへアクセスし、Cloudwatch へPutMetricする。後はグラフ化するなりダッシュボード化するなり色々
+# 管理画面のURLは下記に指定
+
 from bs4 import BeautifulSoup
 import re
 from pprint import pprint
 import requests
 import boto3
 import datetime
+
 def send_metrics(name,val):
     region = 'us-east-1'
     cloudwatch = boto3.client('cloudwatch', region_name=region)
@@ -25,7 +29,8 @@ def send_metrics(name,val):
 def extract_numbers_from_table():
     # BeautifulSoupを使用してHTMLを解析
     #response = requests.get("https://tomita.s3.ap-northeast-1.amazonaws.com/packet.html")
-    response = requests.get("http://localhost:8888/packet.html")
+    url = "http://localhost:8888/packet.html"
+    response = requests.get(url)
     response.raise_for_status()
     source_encoding = response.encoding
     html = response.content.decode(source_encoding).encode("utf-8").decode("utf-8")
